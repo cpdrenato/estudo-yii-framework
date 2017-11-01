d5880709b183b0f663a4d271d7f57c306f39d57d
cpdrenato <cpdenato@gmail.com>
2017-11-01 14:39:09 -0200

    1 contato

---
+++ protected/views/site/index.php
@@ -0,0 +1,16 @@
+<?php
+/* @var $this SiteController */
+
+$this->pageTitle=Yii::app()->name;
+?>
+
+<h1>Bem Vindo a <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
+
+<p>Pagina em Yii Framework creada com sucesso!</p>
+
+
+
+<p>Para mais detalhes sobre a forma de desenvolver esta aplicação, por favor leia
+as <a href="http://www.yiiframework.com/doc/">documentation</a>.
+Sinta-se livre para perguntar no <a href="http://www.yiiframework.com/forum/">forum</a>,
+se você tiver quaisquer perguntas.</p>
